9e3312b05533487f84f4adf715acfc66110e6656
MalleusMus <m.malleus@gmail.com>
2016-03-30 17:17:16 +0300

    end

---
+++ MyCar/Assets/Scripts/Camera/CarOrbitCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyCar {
+    public class CarOrbitCamera : MonoBehaviour {
+
+        public Transform m_Pivot;
+        public float m_OrbitSpeed = 2.5f;
+        public float m_MouseWheelSpeed = 1.5f;
+
+        private float m_ZoomPosition = -10.0f;
+        private float m_RotationX = 30.0f;
+        private float m_RotationY = 0.0f;
+
+
+        // Use this for initialization
+        void Start(){
+            if (m_Pivot == null) m_Pivot = transform.parent;
+        }
+
+        // Update is called once per frame
+        void Update(){
+            m_RotationX -= Input.GetAxis("Mouse Y") * m_OrbitSpeed;
+            m_RotationY += Input.GetAxis("Mouse X") * m_OrbitSpeed;
+            m_ZoomPosition += Input.GetAxis("MouseWheel") * m_MouseWheelSpeed;
+
+            m_Pivot.transform.localRotation = Quaternion.Euler(m_RotationX, m_RotationY, 0.0f);
+            transform.localPosition = new Vector3(0.0f, 0.0f, m_ZoomPosition);
+        }
+    }
+}
---
+++ MyCar/Assets/Scripts/Car/CarControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace MyCar {
+    class CarControl : MonoBehaviour {
+
+        public float speed = 2.0f;
+        public WheelCollider frontLeft = null;
+        public WheelCollider frontRight = null;
+        public WheelCollider backLeft = null;
+        public WheelCollider backRight = null;
+
+        public void Start() {
+
+        }
+
+        public void Update() {
+            
+        }
+
+        public void FixedUpdate() {
+            float torque = 0.0f;
+            torque = Input.GetAxis("AxisV") * speed;
+
+            frontLeft.motorTorque = torque;
+            frontRight.motorTorque = torque;
+            backLeft.motorTorque = torque;
+            backRight.motorTorque = torque;
+        }
+    }
+}
